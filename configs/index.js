551e477f3bbd58ed128a2c4f9942175c7e908f56
ziwon <soch7188@naver.com>
2018-01-19 13:37:23 +0900

    restructuring

--- configs/index.js
+++ configs/index.js
@@ -4,6 +4,6 @@ const
 	_ = require('lodash'),
 	env = process.env.NODE_ENV || 'local',
 	envConfig = require('./' + env),
-	dbKeyConfig = require('../db_and_keys');
+	dbKeyConfig = require('./db_and_keys');
 
 module.exports = _.merge(envConfig, dbKeyConfig);
