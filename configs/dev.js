41579bcd8ee463242e967aac59f6220c6074571e
Taegyun Um <tg.um@youngwoo.co>
2018-02-23 13:31:09 +0900

    토큰 도메인 값 변경

--- configs/dev.js
+++ configs/dev.js
@@ -3,7 +3,7 @@
 let devConfig = {
 	env: 'dev',
 	port: 6001,
-	token_domain: '.jellylab.io',
+	token_domain: 'devatopy.jellylab.io',
 };
 
 module.exports = devConfig;
--- configs/production.js
+++ configs/production.js
@@ -3,7 +3,7 @@
 let productionConfig = {
 	env: 'production',
 	port: 6011,
-	token_domain: '.jellylab.io',
+	token_domain: 'atopy.jellylab.io',
 };
 
 module.exports = productionConfig;
