a33fe767bd29a0a3625a3c24c56835b386c538cf
Taegyun Um <wglsa2@naver.com>
2018-03-14 21:40:28 +0900

    verifyDoctorCode API 추가

--- server/controllers/apis/patients/index.js
+++ server/controllers/apis/patients/index.js
@@ -4,34 +4,40 @@ const
     express = require('express'),
     patientService = require('../../../services/patients'),
     models = require('../../../models'),
-    chatbotService = require('../../../services/patients/chatbot')
+    chatbotService = require('../../../services/patients/chatbot');
 
 let router = express.Router();
 
-console.log('apis/patients/index.js called')
+console.log('apis/patients/index.js called');
 
 // Special request for Dashboard Chart URL for patients from Kakao Chatbot Server.
-router.get('/get_patient_chart_url/:encrypted_kakao_id', chatbotService.getPatientChartURL)
+router.get('/get_patient_chart_url/:encrypted_kakao_id', chatbotService.getPatientChartURL);
 
 // ------ Kakao chatbot requests ------ //
 // TODO: The below APIs do not follow the convention of making use of POST GET PUT DELETE for general purposes.
 // Therefore, the naming may need to be updated in the future.
 // For example, router.post('/register_patient') => router.post('/patient') and so on.
-router.post('/register_patient', chatbotService.registerPatient)
-router.post('/update_patient', chatbotService.updatePatient)
-router.post('/update_daily', chatbotService.updateDaily)
-router.post('/create_patient_log', chatbotService.createPatientLog)
-router.post('/update_exit', chatbotService.updateExit)
-router.get('/get_patient_info/:kakao_id', chatbotService.getPatientInfo)
-router.get('/get_medicine_time/:kakao_id', chatbotService.getMedicineTime)
-router.post('/create_medicine_time', chatbotService.createMedicineTime)
-router.get('/get_medicine_check/:kakao_id', chatbotService.getMedicineCheck)
-router.post('/create_medicine_check', chatbotService.createMedicineCheck)
 
-router.post('/create_mood_check', chatbotService.createMoodCheck)
-router.post('/create_mood_check_text', chatbotService.createMoodCheckText)
+/**
+ * api/v1/patients/
+ */
 
-router.get('/get_medicine_time_to_check/:kakao_id/:time', chatbotService.getMedicineTimeToCheck)
+router.post('/register_patient', chatbotService.registerPatient);
+router.post('/verify_doctor_code', chatbotService.verifyDoctorCode);
+router.post('/update_patient', chatbotService.updatePatient);
+router.post('/update_daily', chatbotService.updateDaily);
+router.post('/create_patient_log', chatbotService.createPatientLog);
+router.post('/update_exit', chatbotService.updateExit);
+router.get('/get_patient_info/:kakao_id', chatbotService.getPatientInfo);
+router.get('/get_medicine_time/:kakao_id', chatbotService.getMedicineTime);
+router.post('/create_medicine_time', chatbotService.createMedicineTime);
+router.get('/get_medicine_check/:kakao_id', chatbotService.getMedicineCheck);
+router.post('/create_medicine_check', chatbotService.createMedicineCheck);
+
+router.post('/create_mood_check', chatbotService.createMoodCheck);
+router.post('/create_mood_check_text', chatbotService.createMoodCheckText);
+
+router.get('/get_medicine_time_to_check/:kakao_id/:time', chatbotService.getMedicineTimeToCheck);
 
 
 router.get('/get_patients', patientService.getPatients);
