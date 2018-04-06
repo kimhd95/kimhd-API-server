74e1685e806d8a01897ad48119aea6c05785f7c5
Taegyun Um <tg.um@youngwoo.co>
2018-04-06 09:33:32 +0900

    미세먼지 정보에 DB에 기록

--- server/services/batch/index.js
+++ server/services/batch/index.js
@@ -10,31 +10,88 @@
 
 const models = require('../../models');
 const Op = require('sequelize').Op;
+const async = require('async');
 var cron = require('node-cron');
 var request = require('request');
+var moment = require('moment');
 var logger = require('../../config/winston');
+
+let serviceKey = 'AuD1lSRzmSkPUA8g67GQ7j401ZJUfoehNzRCdam9Kvh37WR1klQgqoLKrdjvWR8RvuCvXe991jEcjAzn%2BjyPJA%3D%3D';
+let numOfRows = '10';
+let pageSize = '10';
+
 
 function startMicroDustBatch() {
     logger.info("MicroDustBatch started !!");
 
-    cron.schedule('*/10 * * * * *', function () {
-        logger.debug('running MicroDustAPI call every 5:00 AM / ' + new Date());
+    cron.schedule('* * 5 * * *', function () {
+        logger.info('running MicroDustAPI call every 5:00 AM / ' + new Date());
 
-        let serviceKey = 'AuD1lSRzmSkPUA8g67GQ7j401ZJUfoehNzRCdam9Kvh37WR1klQgqoLKrdjvWR8RvuCvXe991jEcjAzn%2BjyPJA%3D%3D';
-        let numOfRows = '10';
-        let pageSize = '10';
-        let itemCode = 'PM10';
+        let tasks = [
+            // PM10
+            function(cb) {
+                getDustValue('PM10',(err, result) => {
+                    if(err) {
+                        cb(err);
+                    } else {
+                        cb(null, result)
+                    }
+                });
+            },
+            // PM25
+            function(cb) {
+                getDustValue('PM25',(err, result) => {
+                    if(err) {
+                        cb(err);
+                    } else {
+                        cb(null, result)
+                    }
+                });
+            },
+        ];
 
-        let url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst?serviceKey='+serviceKey+'&numOfRows='+numOfRows+
-            '&pageSize='+pageSize+'&itemCode='+itemCode+'&dataGubun=DAILY&searchCondition=WEEK';
-
-        request(url, function (error, response, body) {
-            logger.error('error:', error); // Print the error if one occurred
-            logger.debug('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            logger.debug('body:', body); // Print the HTML for the Google homepage.
-        });
+        async.parallel(tasks,(err, result) => {
+            if (err) {
+                logger.error("error : ",err);
+            } else {
+                // Write to DB
+                models.Weather.create({
+                    date: moment().subtract(1,'days').format('YYYY-MM-DD'),
+                    pm10: result[0],
+                    pm25: result[1]
+                }).then(result => {
+                    logger.debug("Weather inserted successfully : ",JSON.stringify(result,null,2));
+                }).catch(function (err){
+                    logger.error("Error while inserting weather DB :", JSON.stringify(err,null,2));
+                });
+            }
+        })
     }).start();
 };
+
+function getDustValue(itemCode, cb) {
+    let url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst?serviceKey='+serviceKey+'&numOfRows='+numOfRows+
+        '&pageSize='+pageSize+'&itemCode='+itemCode+'&dataGubun=DAILY&searchCondition=WEEK&_returnType=json';
+
+    request(url, function (error, response, body) {
+        if(error) cb('Error requesting dust API : '+error);
+        if(response.statusCode == '200') {
+            let result = JSON.parse(body);
+
+            let seoul = result.list[0].seoul;
+            let dataTime = result.list[0].dataTime;
+            let yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD');
+
+            if(dataTime == yesterday) {
+                cb(null, seoul);
+            } else {
+                cb('dataTime is wrong : '+dataTime);
+            }
+        } else {
+            cb("Unexpected Status for dust API : "+response.statusCode);
+        }
+    });
+}
 
 module.exports = {
     startMicroDustBatch:startMicroDustBatch
