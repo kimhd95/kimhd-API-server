d0bdb43828bffc370788dbbb1705d7bb74edde01
Nari Ryu <nariu@jellylab.io>
2018-09-04 16:51:22 +0900

    winston 삭제

--- server/controllers/apis/patients/index.js
+++ server/controllers/apis/patients/index.js
@@ -2,35 +2,35 @@
 
 const
     express = require('express'),
-    patientService = require('../../../services/patients'),
+    userService = require('../../../services/users'),
     models = require('../../../models'),
-    chatbotService = require('../../../services/patients/chatbot');
+    chatbotService = require('../../../services/users/chatbot');
 
 let router = express.Router();
 
-console.log('apis/patients/index.js called');
+console.log('apis/users/index.js called');
 
-// Special request for Dashboard Chart URL for patients from Kakao Chatbot Server.
-router.get('/get_patient_chart_url/:encrypted_kakao_id', chatbotService.getPatientChartURL);
+// Special request for Dashboard Chart URL for users from Kakao Chatbot Server.
+router.get('/get_user_chart_url/:encrypted_kakao_id', chatbotService.getPatientChartURL);
 
 // ------ Kakao chatbot requests ------ //
 // TODO: The below APIs do not follow the convention of making use of POST GET PUT DELETE for general purposes.
 // Therefore, the naming may need to be updated in the future.
-// For example, router.post('/register_patient') => router.post('/patient') and so on.
+// For example, router.post('/register_user') => router.post('/user') and so on.
 
 /**
- * api/v1/patients/
+ * api/v1/users/
  */
 
-router.post('/register_patient', chatbotService.registerPatient);
+router.post('/register_user', chatbotService.registerPatient);
 router.post('/verify_doctor_code', chatbotService.verifyDoctorCode);
-router.post('/update_patient', chatbotService.updatePatient);
+router.post('/update_user', chatbotService.updatePatient);
 router.post('/update_daily', chatbotService.updateDaily);
 router.post('/update_stamp', chatbotService.updateStamp);
-router.post('/create_patient_image', chatbotService.createPatientImage);
-router.post('/create_patient_log', chatbotService.createPatientLog);
+router.post('/create_user_image', chatbotService.createPatientImage);
+router.post('/create_user_log', chatbotService.createPatientLog);
 router.post('/update_exit', chatbotService.updateExit);
-router.get('/get_patient_info/:kakao_id', chatbotService.getPatientInfo);
+router.get('/get_user_info/:kakao_id', chatbotService.getPatientInfo);
 router.get('/get_restaurant_info/:kakao_id', chatbotService.getRestaurantInfo);
 router.get('/get_medicine_time/:kakao_id', chatbotService.getMedicineTime);
 router.post('/create_medicine_time', chatbotService.createMedicineTime);
@@ -43,20 +43,20 @@ router.post('/create_mood_check_text', chatbotService.createMoodCheckText);
 router.get('/get_medicine_time_to_check/:kakao_id/:time', chatbotService.getMedicineTimeToCheck);
 
 
-router.get('/get_patients', patientService.getPatients);
-router.get('/:id', patientService.getPatientWithId);
+router.get('/get_users', userService.getPatients);
+router.get('/:id', userService.getPatientWithId);
 
 
 router.use('/*', models.verifyAPIKEY);
 // ^Middleware. Make sure to put all the routes which needs authentication below this middleware.
 
-router.post('/register_doctor_code', patientService.registerDoctorCode);
-router.post('/kakao_text', patientService.kakaoText);
-router.post('/medicine_check', patientService.medicineCheck);
-router.post('/medicine_check_miss_reason', patientService.medicineCheckMissReason);
-router.post('/medicine_check_med_side', patientService.medicineCheckMedSide);
-//router.post('/medicine_check_med_side_degree', patientService.medicineCheckMedSideDegree);
-router.post('/mood_check', patientService.moodCheck);
-router.post('/mood_check_reason', patientService.moodCheckReason);
+router.post('/register_doctor_code', userService.registerDoctorCode);
+router.post('/kakao_text', userService.kakaoText);
+router.post('/medicine_check', userService.medicineCheck);
+router.post('/medicine_check_miss_reason', userService.medicineCheckMissReason);
+router.post('/medicine_check_med_side', userService.medicineCheckMedSide);
+//router.post('/medicine_check_med_side_degree', userService.medicineCheckMedSideDegree);
+router.post('/mood_check', userService.moodCheck);
+router.post('/mood_check_reason', userService.moodCheckReason);
 
 module.exports = router;
