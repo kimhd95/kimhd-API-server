4ccdccf2745d4032033d1191b2291811af6cb476
tugur <tugur@naver.com>
2018-11-22 15:51:53 +0900

    doctor 관련 삭제

--- server/routes/apis/v1/index.js
+++ server/routes/apis/v1/index.js
@@ -2,12 +2,10 @@
 
 const
 	express = require('express'),
-	doctorsController = require('../../../controllers/apis/doctors'),
 	usersController = require('../../../controllers/apis/users');
 
 let router = express.Router();
 
-router.use('/doctors', doctorsController) // Doctor Dashboard Web App
 router.use('/users', usersController) // Chatbot Server
 
 module.exports = router
