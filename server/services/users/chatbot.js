b499d23a9269e457ecce4f56fb8bdd2cd199b898
Kim Hyun Dong <kimhd10@naver.com>
2019-03-28 20:19:04 +0900

    add log

--- server/services/users/chatbot.js
+++ server/services/users/chatbot.js
@@ -1043,7 +1043,7 @@ function getNearRestaurant (req, res) {
   let price_dinner = req.body.price_dinner;
   let lat = req.body.lat;
   let lng = req.body.lng;
-
+  console.log(price_lunch, price_dinner);
   let price_lunch_flag = '';
   let price_dinner_flag = '';
 
