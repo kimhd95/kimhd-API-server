d46e61f39ac50a723afa72d226498b7295829cba
Kim Hyun Dong <kimhd10@naver.com>
2019-04-17 14:54:06 +0900

    술집고르기 쿼리 수정 테스트-4

--- server/services/users/chatbot.js
+++ server/services/users/chatbot.js
@@ -3614,8 +3614,8 @@ WHERE date=(SELECT MAX(date) FROM decide_histories WHERE subway = p.subway AND e
      drink_type = drink_type.replace('맥주','생맥주, 병맥주');
    }
 
-   console.log(`drink_round : ${drink_round}, price_dinner_flag : ${price_dinner_flag}, price_dinner : ${price_dinner},
-     mood2_flag : ${mood2_flag}, mood2 : ${mood2}, mood_flag : ${mood_flag}, mood : ${mood}, drink_type : ${drink_type}`);
+   // console.log(`drink_round : ${drink_round}, price_dinner_flag : ${price_dinner_flag}, price_dinner : ${price_dinner},
+   //   mood2_flag : ${mood2_flag}, mood2 : ${mood2}, mood : ${mood}, drink_type : ${drink_type}`);
    //lng, lat 값이 있는 경우
    if (lng != null && lat != null) {
      let query = `SELECT * FROM restaurants WHERE closedown=0 AND
