f60dde286c8706c11ea8daf3045c23b55477f83d
Kim Hyun Dong <kimhd10@naver.com>
2019-05-02 19:25:18 +0900

    오류수정

--- server/services/users/chatbot.js
+++ server/services/users/chatbot.js
@@ -3892,7 +3892,7 @@ function getChelinguideList(req, res) {
     setTimeout(() => {
       console.log(imgArray);
       return res.status(200).json({success: true, num: result[0].length, message: result[0], image: imgArray});
-    }, 5000});
+    }, 5000);
   }).catch(err => {
     return res.status(500).json({success: false, message: 'Internal Server or Database Error. err: ' + err.message});
   });
